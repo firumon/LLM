520eae776937843709d602f1a52e35e6bd9672e5
Firose Hussain <github@firumon.com>
2019-03-10 11:47:06 +0530

    init

---
+++ src/LLMServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Firumon\LLM;
+
+use Illuminate\Support\ServiceProvider;
+
+class LLMServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
